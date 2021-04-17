4156a1a766b44cd1e0dcabb021d6ac7bee629a57
MavsPeterKE <mavspeter@gmail.com>
2021-04-17 11:16:29 +0300

    Updated Ngrok Link

--- app/src/main/java/com/example/arcgbot/utils/Constants.java
+++ app/src/main/java/com/example/arcgbot/utils/Constants.java
@@ -2,7 +2,7 @@ package com.example.arcgbot.utils;
 
 public class Constants {
 
-    public static final String BASE_URL   = "https://eff48534309b.ngrok.io";
+    public static final String BASE_URL   = "https://ef3df734d9f7.ngrok.io";
     public static final String SUCCESS = "success";
     public static final String  GENERIC_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
     public static final String  DATE_FORMAT = "yyyy-MM-dd";
