8625c232422c4260ed344f2eb271fe8144af5776
MavsPeterKE <mavspeter@gmail.com>
2021-06-23 14:16:38 +0300

    WIP

--- app/src/main/java/com/example/arcgbot/utils/FirebaseLogs.java
+++ app/src/main/java/com/example/arcgbot/utils/FirebaseLogs.java
@@ -1,2 +1,25 @@
-package com.example.arcgbot.utils;public class FirebaseLogs {
+package com.example.arcgbot.utils;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import javax.inject.Inject;
+
+public class FirebaseLogs {
+    private DatabaseReference firebaseDatabaseReference;
+
+    @Inject
+    public FirebaseLogs() {
+        firebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
+
+    }
+
+   /* public void setLog(String date, String screenId, T value) {
+        firebaseDatabaseReference.child("logs")
+                .child(date)
+                .child(screenId)
+                  *//*  .child(logTitle)
+                    .setValue(logStructure);*//*
+    }*/
+
 }
