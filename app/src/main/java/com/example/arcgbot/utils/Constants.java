02d03eb4af9f0b50aab9858b9cbdcf851a837495
MavsPeterKE <mavspeter@gmail.com>
2021-09-06 10:48:51 +0300

    Added UI Fixes

--- app/src/main/java/com/example/arcgbot/utils/Constants.java
+++ app/src/main/java/com/example/arcgbot/utils/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final String SUCCESS = "success";
     public static final String GENERIC_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
     public static final String DATE_FORMAT = "yyyy-MM-dd";
-    public static final String DEFAULT_USER = "juja_cross_roads_test";
+    public static final String DEFAULT_USER = "juja_cross_roads";
 
     public static String getBaseUrl() {
         String url = Prefs.getString(PrefsKeys.BASE_URL);
